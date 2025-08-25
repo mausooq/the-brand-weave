86d106f907de1415dba432151bedc1ff2084c5b8
shanks-2024 <4pa21cs057@pace.edu.in>
2025-08-25 16:43:45 +0530

    Added social links

--- footer.php
+++ footer.php
@@ -70,9 +70,9 @@ $footer_links = [
         </form>
         <h6 class="mb-4 mt-5" style="color: #122c24;">OUR SOCIALS</h6>
         <div class="social-links">
-          <a href="#" title="Instagram"><i class="bi bi-instagram"></i></a>
+          <a href="https://www.instagram.com/thebrandweave_/" title="Instagram"><i class="bi bi-instagram"></i></a>
           <a href="#" title="LinkedIn"><i class="bi bi-linkedin"></i></a>
-          <a href="#" title="Facebook"><i class="bi bi-facebook"></i></a>
+          <a href="https://www.facebook.com/thebrandweave/?notif_id=1755704661385230&notif_t=follower_invite_accept&ref=notif" title="Facebook"><i class="bi bi-facebook"></i></a>
           <a href="#" title="Twitter"><i class="bi bi-twitter-x"></i></a>
         </div>
       </div>
